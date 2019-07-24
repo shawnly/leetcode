e984390fe069879d999ebef233d079a69ec2cf1d
shawnly@hotmail.com <Shawn@Shawn-Lis-MacBook-Pro.local>
2019-07-23 23:09:31 -0700

    create maven project

---
+++ src/main/java/testbase/leetcode/App.java
@@ -0,0 +1,13 @@
+package testbase.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
